7f88903ac4fe5ae9e6ce33e83bc3a7360d4c4359
Brecht Verhoeve <bverhoeve@deloitte.com>
2020-04-08 17:56:10 +0200

    API skeleton code

---
+++ start_server.sh
@@ -0,0 +1,2 @@
+export FLASK_APP='viper/app.py'
+python -m flask run
---
+++ viper/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  return 'Hello from Viper!'
+
+# To implement GET/ping
+@app.route('/ping')
+def ping():
+  raise NotImplementedError
+
+# To implement POST/start
+@app.route('/start')
+def start():
+  raise NotImplementedError
+
+# To implement POST/move
+@app.route('/move')
+def move():
+  raise NotImplementedError
+
+# To implement POST/end
+@app.route('/end')
+def end():
+  raise NotImplementedError
