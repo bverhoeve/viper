58d49ef07b264cbda10324f4d7b92dfb3ed6aae9
Brecht Verhoeve <bverhoeve@deloitte.com>
2020-04-08 17:50:09 +0200

    setup of package

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+  long_description = fh.read()
+
+with open('requirements.txt', 'r') as requirements_file:
+  requirements = requirements_file.read().splitlines()
+
+setuptools.setup(
+  name='viper',
+  version='0.0.1',
+  author='bverhoeve',
+  author_email='brecht.verhoeve@telenet.be',
+  description='A BattleSnake viper',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages=setuptools.find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'Operating System :: OS Independent',
+  ],
+  install_requires=requirements
+)
