b3482a911d1c96211c25d4cf4660b767acd6eb27
Brecht Verhoeve <bverhoeve@deloitte.com>
2020-04-12 17:47:51 +0200

    Remove hack 'n slash code

--- viper/app.py
+++ viper/app.py
@@ -26,12 +26,3 @@ def move():
 @app.route('/end')
 def end():
   raise NotImplementedError
-
-if __name__ == '__main__':
-  if os.environ['ENV'] == 'production':
-    port = 80
-  else:
-    port = 5000
-  
-  app.run(host='0.0.0.0', port=port)
-
